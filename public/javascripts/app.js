e063cccda7b91e703ad6bced335cdc946fa570c0
Frank Gonzalez <frank.gonzalez628@gmail.com>
2017-01-06 11:54:01 -0600

    angular is broken, will fix shortly

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,9 @@
-var app = angular.module('app', [])
+var app = angular.module('app', ['ngAnimate', 'ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/form.html',
+            controller: 'myController'
+        })
+});
