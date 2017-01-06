b37eefc3f0e2b00829f02bb5506286a6537f8f86
Frank Gonzalez <frank.gonzalez628@gmail.com>
2017-01-06 11:10:45 -0600

    express setup, only going off base angular, no controllers created yet

--- app.js
+++ app.js
@@ -1,60 +1,40 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-var app = express();
+const environment = process.env.NODE_ENV;
+const cookieSession = require('cookie-session')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(methodOverride('_method'))
+
+app.use(cookieSession({
+    secret: 'alfred',
+}));
+
+app.use(express.static(__dirname + '/public'));
+
+const routes = require('./routes');
+
+app.get('/', function(req, res) {
+    res.render('./public/index');
+});
 
 app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+app.listen(PORT, function() {
+    console.log('Hello from port:', PORT);
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
