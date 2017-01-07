54b08738f7907c1b69b97cb911b8b12789bd6229
Sam Rosenbluth <samuel.rosenbluth@gmail.com>
2017-01-07 15:20:56 -0600

    merge conflict

--- public/javascripts/controllers/myController.js
+++ public/javascripts/controllers/myController.js
@@ -3,4 +3,14 @@ app.controller('myController', ['$scope', '$http', function($scope, $http) {
     $scope.view = {};
     $scope.view.booyah = 'booyah x 100';
 
+    $http({
+        method: 'GET',
+        url: 'http://localhost:9090/partners'
+    }).then(function successCallback(response) {
+        console.log(response.data);
+    }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+    });
+
 }]);
