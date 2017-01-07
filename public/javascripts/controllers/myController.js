f5349db7456fb6fbc3be4b8e385dfcf9be7fe5dd
Sam Rosenbluth <samuel.rosenbluth@gmail.com>
2017-01-07 15:23:25 -0600

    merge

--- public/javascripts/controllers/myController.js
+++ public/javascripts/controllers/myController.js
@@ -13,4 +13,14 @@ app.controller('myController', ['$scope', '$http', function($scope, $http) {
         // or server returns response with an error status.
     });
 
+    $http({
+        method: 'GET',
+        url: 'http://localhost:9090/partners'
+    }).then(function successCallback(response) {
+        console.log(response.data);
+    }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+    });
+
 }]);
