aa942d6d34a4b8cdd4217135354e9c376bd78b0d
Frank Gonzalez <frank.gonzalez628@gmail.com>
2017-01-09 16:22:41 -0600

    Partner service up and running, GET routes written for partner and referral

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var Referral = require('../backend/models/referral.js');
+var Partner = require('../backend/models/partner.js');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('../public/index', {
-        title: 'Express'
-    });
+
+
+router.get('/api/partners', function(req, res) {
+    Partner.find().then(function(err, partners) {
+        if (err) {
+            res.send(err);
+            console.log(err);
+        } else {
+            console.log(partners + "FROM ROUTE");
+            res.send(partners);
+        }
+    })
 });
 
-module.exports = router;
+router.get('/api/referrals', function(req, res) {
+    Referral.find(function(err, referrals) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(referrals);
+        }
+    })
+});
+
+
+
+module.exports = router;;
