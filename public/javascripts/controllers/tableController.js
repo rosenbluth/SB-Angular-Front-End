478304113f7825bb66dce14c3231b5ecd5bacbc8
Frank Gonzalez <frank.gonzalez628@gmail.com>
2017-01-11 14:45:53 -0600

    payout date added to table

--- public/javascripts/controllers/tableController.js
+++ public/javascripts/controllers/tableController.js
@@ -7,14 +7,28 @@ app.controller('tableController', ['$scope', '$http', '$location', '$window', 'C
     $scope.currentUser.email = $window.localStorage.getItem('email');
     console.log($scope.currentUser.email, 'currentUserEmail');
 
+    //Get request to API and returns referrals for logged in partner, sets to data variable
     var request = $http.get('/api/partners/' + $scope.currentUser.email + '/referrals').then(function(response) {
         //need to plug these values in to table
-        // console.log(response.data, 'asdasd response');
-        $scope.data = response.data;
+        console.log(response.data[1], 'asdasd response');
+        $scope.data = response.data[1]; //array of referrals for this partner
         return response.data;
-        console.log(response.data, 'test response.data');
     });
 
+    //Date information and view for payout date
+    $scope.date = new Date().getMonth() + 1;
+    $scope.payoutDate = function() {
+
+        if (1 <= $scope.date <= 3) {
+            $scope.date = 'April 1st';
+        } else if (4 <= $scope.date <= 6) {
+            $scope.date = 'July 1st';
+        } else if (7 <= $scope.date <= 9) {
+            $scope.date = 'October 1st';
+        } else {
+            $scope.date = 'January 1st';
+        }
+    }();
 
 
 
