54f002cb4ae3d23da7e694bd83bcc323ec5fad13
Frank Gonzalez <frank.gonzalez628@gmail.com>
2017-01-12 21:30:06 -0600

    README added

--- public/javascripts/controllers/tableController.js
+++ public/javascripts/controllers/tableController.js
@@ -1,5 +1,4 @@
-
-app.controller('tableController', ['$scope', '$http', '$location', '$window', 'CurrentPartner', 'LogOutService',function($scope, $http, $location, $window, CurrentPartner, LogOutService) {
+app.controller('tableController', ['$scope', '$http', '$location', '$window', 'CurrentPartner', 'LogOutService', function($scope, $http, $location, $window, CurrentPartner, LogOutService) {
 
     $scope.logout = LogOutService;
 
@@ -10,7 +9,7 @@ app.controller('tableController', ['$scope', '$http', '$location', '$window', 'C
 
 
 
-    $scope.convertClicked = function(referral){
+    $scope.convertClicked = function(referral) {
         confirm('You are about to convert a referral to active payout. Are you sure?')
         var referralIndex = this.$index;
 
@@ -24,28 +23,28 @@ app.controller('tableController', ['$scope', '$http', '$location', '$window', 'C
             console.log($scope.data[referralIndex].conversionDate, 'newDate');
         })
 
-        }
+    }
 
 
 
 
-    CurrentPartner().then(function(partnerReturned){
+    CurrentPartner().then(function(partnerReturned) {
         $scope.currentUser = partnerReturned;
-        if($scope.currentUser.email === "cat@cat1.com"){
+        if ($scope.currentUser.email === "cat@cat1.com") {
             $scope.admin = true
-            $http.get('/api/referrals').then(function(response){
+            $http.get('/api/referrals').then(function(response) {
                 // console.log(response.data, 'response.data admin');
                 $scope.data = response.data;
                 console.log($scope.data);
             })
+        } else {
+            $http.get('/api/partners/' + $scope.currentUser.email + '/referrals').then(function(response) {
+                //need to plug these values in to table
+                console.log(response.data[1], 'asdasd response from table cont');
+                $scope.data = response.data[1]; //array of referrals for this partner
+                return response.data;
+            });
         }
-        else{$http.get('/api/partners/' + $scope.currentUser.email + '/referrals').then(function(response) {
-           //need to plug these values in to table
-           console.log(response.data[1], 'asdasd response from table cont');
-           $scope.data = response.data[1]; //array of referrals for this partner
-           return response.data;
-       });
-    }
     })
 
 
@@ -66,23 +65,23 @@ app.controller('tableController', ['$scope', '$http', '$location', '$window', 'C
 
 
 
-    // if ryan clicks convert, this controller makes a new Date(), then makes a
-    // post request to 'api/referrals/converted' route
-    // that route: finds the referral based on id
-    //             updates the referral's starterConverted or growthConverted booleans to true
-    //              updates activeReferral to true
-    //             and sets the referal's conversionDate
-    //     in the dot then:
-    // -- $scope.data[referralIndex].starterConverted or .growthConverted gets set to true, and when it does:
-    // -- $scope.data[referralIndex].totalAmountPaidForReferral will automatically be either 149 or 399, based on above property
-    // -- $scope.data[referralIndex].bonusAmount will automatically be either 149 or 399, based on above property
-    //
+    // DONE DONE DONE DONE DONE if ryan clicks convert, this controller makes a new Date(), then makes a
+    //   post request to 'api/referrals/converted' route
+    //   that route: finds the referral based on id
+    //               updates the referral's starterConverted or growthConverted booleans to true
+    //                updates activeReferral to true
+    //               and sets the referal's conversionDate
+    //       in the dot then:
+    //   -- $scope.data[referralIndex].starterConverted or .growthConverted gets set to true, and when it does:
+    //   -- $scope.data[referralIndex].totalAmountPaidForReferral will automatically be either 149 or 399, based on above property
+    //   -- $scope.data[referralIndex].bonusAmount will automatically be either 149 or 399, based on above property DONE DONE DONE DONE DONE DONE
+
     //  $scope.data[referralIndex].convertedDate will be response.data.conversionDate (a string)
     // --****  $scope.data[referralIndex].numMonthsPassedSinceConversion will be set to 0
     //     *** numMonthsPassedSinceConversion will have to increase/auto update based on page rendering with a momentJS
     //     function that subtracts today's date minus conversionDate. if > 12 months, .activeStarter or .activeGrowth = false
     //     this property also keeps track of a $scope property
-
+    //
     //
     // // -- $scope.data[referralIndex].totalNumOfQuarterlyPayouts will be 0
     // -- $scope.data[referralIndex].amountMadeTowardNextPayout will be :
@@ -91,20 +90,20 @@ app.controller('tableController', ['$scope', '$http', '$location', '$window', 'C
     //
     //     ALSO, for ALL ADD:
     //  MomentJS math: last payout date minus todays date (will return a number of months since last payout date)
-            // if (numMonthsPassedSinceConversion <= 3)    {  totalAmountPaidForReferral AND amountMadeTowardNextPayout both
-            //                                             = (numMonthsPassedSinceConversion  * monthlyStarter or * monthlyGrowth) + bonusAmount }
-            //
-            // if  (4 <= numMonthsPassedSinceConversion <= 6) { totalNumOfQuarterlyPayouts=1;
-            //                                                 amountMadeTowardNextPayout = (numMonthsPassedSinceConversion - totalNumOfQuarterlyPayouts * 3) * monthlyStarter or * monthlyGrowth; totalAmountPaidForReferral +=  amountMadeTowardNextPayout}
-            //
-            // if ( 7 <= numMonthsPassedSinceConversion <= 9) { totalNumOfQuarterlyPayouts=2;
-            //                                                 amountMadeTowardNextPayout = (numMonthsPassedSinceConversion - totalNumOfQuarterlyPayouts * 3) * monthlyStarter or * monthlyGrowth; totalAmountPaidForReferral  +=  amountMadeTowardNextPayout}
-            //
-            // if  (10 <= numMonthsPassedSinceConversion <=12) { totalNumOfQuarterlyPayouts=3;
-            //                                                 amountMadeTowardNextPayout = (numMonthsPassedSinceConversion - totalNumOfQuarterlyPayouts * 3) * monthlyStarter or * monthlyGrowth;
-            //                                                 totalAmountPaidForReferral +=  amountMadeTowardNextPayout }
-            //
-            // else change them to inactive
+    //         if (numMonthsPassedSinceConversion <= 3)    {  totalAmountPaidForReferral AND amountMadeTowardNextPayout both
+    //                                                     = (numMonthsPassedSinceConversion  * monthlyStarter or * monthlyGrowth) + bonusAmount }
+    //
+    //         if  (4 <= numMonthsPassedSinceConversion <= 6) { totalNumOfQuarterlyPayouts=1;
+    //                                                         amountMadeTowardNextPayout = (numMonthsPassedSinceConversion - totalNumOfQuarterlyPayouts * 3) * monthlyStarter or * monthlyGrowth; totalAmountPaidForReferral +=  amountMadeTowardNextPayout}
+    //
+    //         if ( 7 <= numMonthsPassedSinceConversion <= 9) { totalNumOfQuarterlyPayouts=2;
+    //                                                         amountMadeTowardNextPayout = (numMonthsPassedSinceConversion - totalNumOfQuarterlyPayouts * 3) * monthlyStarter or * monthlyGrowth; totalAmountPaidForReferral  +=  amountMadeTowardNextPayout}
+    //
+    //         if  (10 <= numMonthsPassedSinceConversion <=12) { totalNumOfQuarterlyPayouts=3;
+    //                                                         amountMadeTowardNextPayout = (numMonthsPassedSinceConversion - totalNumOfQuarterlyPayouts * 3) * monthlyStarter or * monthlyGrowth;
+    //                                                         totalAmountPaidForReferral +=  amountMadeTowardNextPayout }
+    //
+    //         else change them to inactive
     //    *****MAYBE need to call scope.apply at the end of function here, not sure
 
 
