af11a863468af81f1e4d5284cb8bec27069f59bc
Jeannie Yamakawa <jkyamakawa@gmail.com>
2017-01-15 19:02:45 -0600

    removes the requiring dot env line

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-require('dotenv').config()
+// require('dotenv').config()
 const PORT = process.env.PORT || 3000;
 
 const environment = process.env.NODE_ENV;
