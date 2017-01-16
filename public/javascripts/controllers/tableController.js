06e00a818aaa7f26ef2b1c3b3538f39a18502734
Jeannie Yamakawa <jkyamakawa@gmail.com>
2017-01-15 21:32:27 -0600

    table controller cleanup

--- public/javascripts/controllers/tableController.js
+++ public/javascripts/controllers/tableController.js
@@ -27,7 +27,6 @@ app.controller('tableController', [
         }
 
         $scope.convertClicked = function(referral) {
-            // confirm('You are about to convert a referral to active payout. Are you sure?')
             var referralIndex = this.$index;
 
             console.log(referral, 'referral argument');
@@ -37,7 +36,6 @@ app.controller('tableController', [
                 $scope.currentUser.data[referralIndex] = response.data;
             })
         }
-        // moment().from($scope.currentUser.data[referralIndex].conversionDate)
 
         CurrentPartner().then(function(partnerReturned) {
             $scope.currentUser = partnerReturned;
@@ -66,25 +64,24 @@ app.controller('tableController', [
         $scope.payoutDate = function() {
 
             if (1 <= $scope.date <= 3) {
-                $scope.date = 'April 1st';
+                $scope.date = 'April 1';
             } else if (4 <= $scope.date <= 6) {
-                $scope.date = 'July 1st';
+                $scope.date = 'July 1';
             } else if (7 <= $scope.date <= 9) {
-                $scope.date = 'October 1st';
+                $scope.date = 'October 1';
             } else {
-                $scope.date = 'January 1st';
+                $scope.date = 'January 1';
             }
         }();
+        $scope.year = new Date().getFullYear();
 
         function makeCalculations(ref) {
             var todaysDate = moment(new Date()).format('YYYY-MM-DD');
             if (!ref.conversionDate) {
                 ref.numMonthsPassedSinceConversion = 0;
             } else {
-
                 ref.numMonthsPassedSinceConversion = moment(todaysDate).diff(moment(ref.conversionDate));
                 ref.totalNumOfQuarterlyPayouts = 0;
-
                 var monthlyCalc = (ref.numMonthsPassedSinceConversion - (ref.totalNumOfQuarterlyPayouts * 3));
                 if (ref.activeReferral && (ref.numMonthsPassedSinceConversion = 0)) {
                     ref.amountMadeTowardNextPayout = ref.bonusAmount;
@@ -128,51 +125,7 @@ app.controller('tableController', [
                     ref.activeReferral = false
                 }
             }
-
             return ref;
-
         }
-
-        // DONE DONE DONE DONE DONE if ryan clicks convert, this controller makes a new Date(), then makes a
-        //   post request to 'api/referrals/converted' route
-        //   that route: finds the referral based on id
-        //               updates the referral's starterConverted or growthConverted booleans to true
-        //                updates activeReferral to true
-        //               and sets the referal's conversionDate
-        //       in the dot then:
-        //   -- $scope.currentUser.data[referralIndex].starterConverted or .growthConverted gets set to true, and when it does:
-        //   -- $scope.currentUser.data[referralIndex].totalAmountPaidForReferral will automatically be either 149 or 399, based on above property
-        //   -- $scope.currentUser.data[referralIndex].bonusAmount will automatically be either 149 or 399, based on above property DONE DONE DONE DONE DONE DONE
-
-        //  $scope.currentUser.data[referralIndex].convertedDate will be response.data.conversionDate (a string)
-        // --****  $scope.currentUser.data[referralIndex].numMonthsPassedSinceConversion will be set to 0
-        //     *** numMonthsPassedSinceConversion will have to increase/auto update based on page rendering with a momentJS
-        //     function that subtracts today's date minus conversionDate. if > 12 months, .activeStarter or .activeGrowth = false
-        //     this property also keeps track of a $scope property
-        //
-        //
-        // // -- $scope.currentUser.data[referralIndex].totalNumOfQuarterlyPayouts will be 0
-        // -- $scope.currentUser.data[referralIndex].amountMadeTowardNextPayout will be :
-        //     if
-        //     numMonthsPassedSinceConversion <=3 it will include the bonusAmount
-        //
-        //     ALSO, for ALL ADD:
-        //  MomentJS math: last payout date minus todays date (will return a number of months since last payout date)
-        //         if (numMonthsPassedSinceConversion <= 3)    {  totalAmountPaidForReferral AND amountMadeTowardNextPayout both
-        //                                                     = (numMonthsPassedSinceConversion  * monthlyStarter or * monthlyGrowth) + bonusAmount }
-        //
-        //         if  (4 <= numMonthsPassedSinceConversion <= 6) { totalNumOfQuarterlyPayouts=1;
-        //                                                         amountMadeTowardNextPayout = (numMonthsPassedSinceConversion - totalNumOfQuarterlyPayouts * 3) * monthlyStarter or * monthlyGrowth; totalAmountPaidForReferral +=  amountMadeTowardNextPayout}
-        //
-        //         if ( 7 <= numMonthsPassedSinceConversion <= 9) { totalNumOfQuarterlyPayouts=2;
-        //                                                         amountMadeTowardNextPayout = (numMonthsPassedSinceConversion - totalNumOfQuarterlyPayouts * 3) * monthlyStarter or * monthlyGrowth; totalAmountPaidForReferral  +=  amountMadeTowardNextPayout}
-        //
-        //         if  (10 <= numMonthsPassedSinceConversion <=12) { totalNumOfQuarterlyPayouts=3;
-        //                                                         amountMadeTowardNextPayout = (numMonthsPassedSinceConversion - totalNumOfQuarterlyPayouts * 3) * monthlyStarter or * monthlyGrowth;
-        //                                                         totalAmountPaidForReferral +=  amountMadeTowardNextPayout }
-        //
-        //         else change them to inactive
-        //    *****MAYBE need to call scope.apply at the end of function here, not sure
-
     }
 ]);
